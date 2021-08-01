ef02ebc25551872f10f42af441896bedc3932726
Charlesopuba <charlesopuba@gmail.com>
2021-08-01 11:26:46 +0300

    added a directive

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -7,29 +7,35 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DateCountPipe implements PipeTransform 
 {
 
-  transform(value: any): number {
-    let today:Date = new Date(); //get current date and time
-    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-    // let to:any = new DatePipe('').transform(value, 'dd:MM:yyyy', 'GMT+1');
-    // let d:any=new Date(value.getFullYear(), value.getMonth(), value.getDate());
-    var dateDifference = Math.abs(todayWithNoTime-value) //returns value in miliseconds
-    const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
-    var dateDifferenceSeconds = dateDifference*0.001; //converts miliseconds to seconds
-    var dateCounter = dateDifferenceSeconds/secondsInDay;
-
-    //&& value > todayWithNoTime
-    if (dateCounter >= 1 )
-    {
-      return dateCounter;
+  transform(value: any, ...args: any[]): any {
+    if(value){
+      const seconds =Math.floor((+new Date()-+new Date(value))/1000);
+      if(seconds<29)
+      return 'just now';
+      const intervals:{[interval:string]:number} ={
+        'year':31536000,
+        'month':2592000,
+        'week':604800,
+        'day':86400,
+        'hour':3600,
+        'minute':60,
+        'second':1
+      };
+      let counter;
+      for(const i in intervals){
+        counter=Math.floor(seconds/intervals[i]);
+        if (counter>0)
+        if(counter===1){
+          return counter+' '+i+' ago';
+        }else{
+          return counter +' '+i+'s ago'
+        }
+      }
     }
-    else if(dateCounter == 0)
-    {
-       return dateCounter;
-    }
-    else{
-      return 0;
-    }
-
+    return value;
   }
-
 }
+
+
+
+
