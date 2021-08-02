0a7902bddae9a8b2198a80229212eeeaa72a69bc
Charlesopuba <charlesopuba@gmail.com>
2021-08-02 07:37:37 +0300

    updated environment access token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   access_token:'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'
 };
 
